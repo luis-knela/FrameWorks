2d78271f2d4a244bef5662b30d262bcdd4142b39
luis-knela <luis.assis@estudante.ifms.edu.br>
2026-06-03 11:59:32 -0400

    faltei

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
 import { AuthModule } from './auth/auth.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [TodosModule, AuthModule],
+  controllers: [AppController],
+  providers: [AppService]
 })
 export class AppModule {}
