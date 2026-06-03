8b0549b9e11054303b70bc199c6aa4c6b4ac060a
luis-knela <luis.assis@estudante.ifms.edu.br>
2026-06-03 11:29:37 -0400

    deliguei o pc de geral

--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TodosModule } from './todos/todos.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TodosModule, AuthModule],
 })
 export class AppModule {}
